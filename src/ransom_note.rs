de3e8b403f4ca5c7fe179ae52fdb5f3a91e3aa97
Yqnk <yan.bertile@gmail.com>
2023-10-12 09:27:39 +0200

    solution "Ransom Note"

--- src/ransom_note.rs
+++ src/ransom_note.rs
@@ -6,7 +6,7 @@ impl Solution {
         for i in 0..magazine.len() {
             magazine_alphabet[magazine.as_bytes()[i] as usize - 97] += 1;
         }
-        println!("{:?}", magazine_alphabet);
+        // println!("{:?}", magazine_alphabet);
         for i in 0..ransom_note.len() {
             let char_pos = ransom_note.as_bytes()[i] as usize - 97;
             if magazine_alphabet[char_pos] > 0 {
