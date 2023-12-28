3bc27f2adf6086914586dca26e31143504489633
Yqnk <yan.bertile@gmail.com>
2023-12-28 13:03:19 +0100

    update

---
+++ src/rs/main.rs
@@ -0,0 +1,5 @@
+// hello world en rust
+
+fn main() {
+    
+}
---
+++ src/rs/valid_parentheses.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        if s.len() % 2 != 0 {
+            return false;
+        }
+        let mut stack: Vec<char> = Vec::new();
+        for c in s.chars() {
+            match c {
+                '(' | '[' | '{' => stack.push(c),
+                ')' => {
+                    if stack.pop() != Some('(') {
+                        return false;
+                    }
+                }
+                ']' => {
+                    if stack.pop() != Some('[') {
+                        return false;
+                    }
+                }
+                '}' => {
+                    if stack.pop() != Some('{') {
+                        return false;
+                    }
+                }
+                _ => return false,
+            }
+        }
+        stack.is_empty()
+    }
+}
