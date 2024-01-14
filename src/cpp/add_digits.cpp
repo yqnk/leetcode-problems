802a97c28770f4df18a7deee5006a175d1092573
Yan <98831646+yqnk@users.noreply.github.com>
2024-01-13 22:15:55 -0500

    Solution "Add Digits"

---
+++ src/cpp/add_digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int r = num;
+        while (r >= 10) {
+            int tmp = r;
+            r = 0;
+            while (tmp > 0) {
+                r += (tmp % 10);
+                tmp /= 10;
+            }
+        }
+        return r;
+    }
+};
